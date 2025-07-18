244e7c233cb2b89cd4c8e83483c56f1cae0a343b
Maf1ck <kosmanmatvij@gmail.com>
2025-07-19 01:37:28 +0300

    123

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,9 +1,11 @@
 import createHttpError from 'http-errors';
 
-export const validateBody = (schema) => (req, res, next) => {
+const validateBody = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body);
   if (error) {
     return next(createHttpError(400, error.details[0].message));
   }
   next();
-}; 
+};
+
+export default validateBody; 
