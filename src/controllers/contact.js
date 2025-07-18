51f5b409463eaced54c3b2168f7617f0baa803f0
Maf1ck <kosmanmatvij@gmail.com>
2025-07-18 09:09:35 +0300

    123

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -9,11 +9,32 @@ import createHttpError from 'http-errors';
 import cloudinary from '../utils/cloudinary.js';
 
 export const getAllContactsController = async (req, res) => {
-  const contacts = await getAllContactsService(req.user._id);
+  const { page = 1, limit = 10, sortBy, sortByDesc, filter } = req.query;
+  const skip = (page - 1) * limit;
+  const sort = {};
+  if (sortBy) sort[sortBy] = 1;
+  if (sortByDesc) sort[sortByDesc] = -1;
+  let filterQuery = { userId: req.user._id };
+  if (filter) {
+    // filter=field1|field2
+    const fields = filter.split('|');
+    filterQuery = {
+      ...filterQuery,
+      $or: fields.map((field) => ({ [field]: { $exists: true } })),
+    };
+  }
+  const { Contact } = await import('../models/contact.js');
+  const contacts = await getAllContactsService(filterQuery, { skip: Number(skip), limit: Number(limit), sort });
+  const total = await Contact.countDocuments(filterQuery);
   res.json({
     status: 200,
     message: 'Successfully found contacts!',
-    data: contacts,
+    data: {
+      total,
+      page: Number(page),
+      limit: Number(limit),
+      contacts,
+    },
   });
 };
 
