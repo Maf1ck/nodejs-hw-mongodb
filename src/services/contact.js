c06b22ff1b54cb9042575a715cd4a24b7f6acbe0
maf1ck <kosmanmatvij@gmail.com>
2025-07-21 21:46:14 +0300

    123

--- src/services/contact.js
+++ src/services/contact.js
@@ -1,6 +1,6 @@
 import { Contact } from '../models/contact.js';
 
-export const getAllContacts = async (userId) => Contact.find({ userId });
+export const getAllContacts = async (filter, options) => Contact.find(filter, null, options);
 export const getContactById = async (contactId, userId) => Contact.findOne({ _id: contactId, userId });
 export const createContact = async (payload) => Contact.create(payload);
 export const deleteContact = async (contactId, userId) => Contact.findOneAndDelete({ _id: contactId, userId });
